ae23779db12ca1c8bad150870518119f64dc2e54
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2017-03-04 09:11:35 +0100

    login added

--- eventlift/views.py
+++ eventlift/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/login')
+def index():
+    return render_template('login.html')
