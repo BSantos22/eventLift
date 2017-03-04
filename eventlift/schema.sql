7d9a10cca158564c071259226abf61a75df87d27
Caramelo18 <fabiocaramelo18@gmail.com>
2017-03-04 04:42:42 +0100

    populated database

--- eventlift/schema.sql
+++ eventlift/schema.sql
@@ -24,8 +24,9 @@ CREATE TABLE Lifts(
     owner REFERENCES Users(rowID),
     event REFERENCES Events(rowID),
     price INTEGER NOT NULL,
-    twoway BOOLEAN NOT NULL,
+    twoway INTEGER NOT NULL,
     lftime VARCHAR(32) NOT NULL,
+    lfplace VARCHAR(32) NOT NULL,
     numseats INTEGER NOT NULL,
     emptyseats INTEGER NOT NULL CHECK(emptyseats > 0)
 );
@@ -42,13 +43,33 @@ CREATE TRIGGER IF NOT EXISTS RemoveEmptySeat
     BEGIN
         UPDATE Lifts
         SET emptyseats = emptyseats - NEW.numseats
-        WHERE (Lifts.rowID = Reservations.event);
+        WHERE (Lifts.rowID = NEW.event);
     END;
 
 CREATE TRIGGER IF NOT EXISTS CancelReservation
     AFTER DELETE ON Reservations
     BEGIN
         Update Lifts
-        SET emptyseats = emptyseats - OLD.numseats
-        WHERE (Lifts.rowID = Reservations.event);
+        SET emptyseats = emptyseats + OLD.numseats
+        WHERE (Lifts.rowID = OLD.event);
     END;
+
+
+
+INSERT INTO Users(user, pass, email, reputation) VALUES("U1", "P1", "asda@asdas.x", 0);
+INSERT INTO Users(user, pass, email, reputation) VALUES("U2", "P2", "asda@asadas.x", 0);
+
+INSERT INTO Events(name, local, stdate, endate) VALUES("PMR PO CRL", "Estádio José Alvalade", "04-03-2017 09:00", "04-03-2017 19:00");
+INSERT INTO Events(name, local, stdate, endate) VALUES("BENFICA É MERDA", "TODO O LADO", "Algures em 1904 (dizem eles)", "PARA SEMPRE");
+
+INSERT INTO Lifts(owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) VALUES(1, 1, 0, 1, "08:55", "PRIMA DO PMR", 100, 100);
+
+SELECT emptyseats FROM Lifts;
+
+INSERT INTO Reservations(event, user, numseats) VALUES(1, 1, 20);
+
+SELECT emptyseats FROM Lifts;
+
+DELETE FROM Reservations WHERE rowID=1;
+
+SELECT emptyseats FROM Lifts;
