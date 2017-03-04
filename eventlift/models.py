6792937077ed7eb011cee532b1319badca4492bd
Diogo Campos <d.diogocampos@gmail.com>
2017-03-04 04:03:53 +0100

    Fixed schema.db path

--- eventlift/models.py
+++ eventlift/models.py
@@ -2,13 +2,15 @@ from eventlift import app
 import sqlite3
 from flask import g
 
-DATABASE = 'schema.db'
+DATABASE = 'eventlift/schema.db'
+
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
 
 @app.teardown_appcontext
 def close_connection(exception):
