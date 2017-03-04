cc6559cac755b0a4f9943e2e7fcfd253392ba293
Diogo Campos <d.diogocampos@gmail.com>
2017-03-04 09:28:42 +0100

    Added basic login functionality

--- eventlift/__init__.py
+++ eventlift/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'di47?Ws.316k4K!afY16xC3#Odh99L'
 
 import eventlift.views
--- eventlift/views.py
+++ eventlift/views.py
@@ -1,10 +1,20 @@
 from eventlift import app, models
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/login')
-def index():
-    return render_template('login.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        registered = models.get_users(username, password)
+        if registered:
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return "Show failed login page"
+    else:
+        return render_template('login.html')
