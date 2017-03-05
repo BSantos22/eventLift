5fde193472071a064c4f592bf0f7ee4504f6ae25
Caramelo18 <fabiocaramelo18@gmail.com>
2017-03-05 08:22:50 +0100

    database updated

--- eventlift/schema.sql
+++ eventlift/schema.sql
@@ -60,23 +60,10 @@ CREATE TRIGGER IF NOT EXISTS CancelReservation
 INSERT INTO Users(user, pass, email, phone, reputation) VALUES("U1", "P1", "asda@asdas.x", 95656589864, 0);
 INSERT INTO Users(user, pass, email, phone, reputation) VALUES("U2", "P2", "asda@asadas.x", 6512132656, 0);
 
-INSERT INTO Events(name, local, stdate, endate) VALUES("PMR PO CRL", "Estádio José Alvalade", "04-03-2017 09:00", "04-03-2017 19:00");
-INSERT INTO Events(name, local, stdate, endate) VALUES("BENFICA É MERDA", "TODO O LADO", "Algures em 1904 (dizem eles)", "PARA SEMPRE");
-
-INSERT INTO Lifts(owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) VALUES(1, 1, 0, 1, "08:55", "PRIMA DO PMR", 100, 100);
+INSERT INTO Events(name, local, stdate, endate) VALUES("Event 1", "UPC", "04-03-2017 09:00", "04-03-2017 19:00");
+INSERT INTO Events(name, local, stdate, endate) VALUES("Event 2", "Airport", "05/03/2017 08:00", "05/03/2017 10:00");
 
 INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 2, 9, 1, '12/23/2016', 'Indonesia', 1, 1);
 INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 1, 47, 1, '6/21/2016', 'Philippines', 7, 1);
 INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 2, 35, 0, '4/12/2016', 'China', 8, 1);
 INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 2, 21, 0, '12/14/2016', 'Poland', 6, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 2, 2, 1, '1/14/2017', 'South Africa', 7, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 1, 35, 1, '2/21/2017', 'Morocco', 6, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 1, 32, 0, '3/17/2016', 'Kuwait', 4, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 2, 46, 1, '7/4/2016', 'Poland', 1, 1);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 2, 13, 1, '2/13/2017', 'Malaysia', 2, 1);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 1, 24, 0, '8/30/2016', 'Nigeria', 3, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 1, 38, 1, '11/15/2016', 'China', 5, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 1, 35, 0, '6/30/2016', 'Venezuela', 5, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 1, 35, 0, '3/2/2017', 'France', 8, 2);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (2, 1, 31, 1, '1/2/2017', 'Japan', 3, 1);
-INSERT INTO Lifts (owner, event, price, twoway, lftime, lfplace, numseats, emptyseats) values (1, 2, 4, 0, '2/7/2017', 'China', 8, 2);
